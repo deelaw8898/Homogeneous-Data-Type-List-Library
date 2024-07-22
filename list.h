246b12a2791e970baf26c8a4c64325bb6ac20726
Waleed Ahmad <waa392@usask.ca>
2024-07-22 13:20:12 -0600

    Adding project files to git

---
+++ list.h
@@ -0,0 +1,64 @@
+/* Author: Waleed Ahmad
+ * This file contains the declarations of the functions 
+ * that are used to create and manipulate a linked list.
+ */
+
+#ifndef __LIST_H__
+#define __LIST_H__
+
+#define ERROR_EXIT -1
+#define SUCCESS_EXIT 0
+
+typedef struct _node {
+	void* item;
+	void* next;
+	void* prev;
+} NODE;
+
+typedef struct _list {
+	NODE* head;
+	NODE* tail;
+	NODE* curr;
+	int count;
+	void* next;
+	void* prev;
+} LIST;
+
+
+typedef void (*itemFree) (void*);
+
+typedef int (*Comparator) (void*, void*);
+
+LIST* ListCreate();
+
+int ListCount(LIST* list);
+
+void* ListFirst(LIST* list);
+
+void* ListLast(LIST* list);
+
+void* ListNext(LIST* list);
+
+void* ListPrev(LIST* list);
+
+void* ListCurr(LIST* list);
+
+int ListAdd(LIST* list, void* item);
+
+int ListInsert(LIST* list, void* item);
+
+int ListAppend(LIST* list, void* item);
+
+int ListPrepend(LIST* list, void* item);
+
+void* ListRemove(LIST* list);
+
+void ListConcat(LIST* list_1, LIST* list_2);
+
+void ListFree(LIST* list, itemFree item_to_be_freed);
+
+void* ListTrim(LIST* list);
+
+void* ListSearch(LIST* list, Comparator comp, void* comparison_arg);
+
+#endif
